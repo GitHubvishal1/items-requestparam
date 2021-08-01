114d9f549a3997472f1af1b34247c4c54e372f81
Vishal Rana <vishal1230rana@gmail.com>
2021-08-02 00:44:44 +0530

    no database

--- src/main/java/com/example/demo/controller/ItemsController.java
+++ src/main/java/com/example/demo/controller/ItemsController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,15 +15,21 @@ public class ItemsController {
 	
 	Map<Integer, String> map;
 	public ItemsController() {
-		// TODO Auto-generated constructor stub
 		map = new HashMap<>();
 		map.put(1, "soap");
 		map.put(2, "toothbrush");
 		map.put(3, "wash");
 	}
 	
-	@GetMapping("/items") //http://localhost:5050/items/
+	@GetMapping("/") //http://localhost:5050/swagger-ui.html
 	public Map<Integer, String> getItems() {
 		return map;
+	}
+	
+	@GetMapping("/get/{tid}") // http://localhost:5050/swagger-ui.html/get/{tid}
+	public List<Entry<Integer, String>> getItems(@PathVariable("tid") int orderId) {
+		return map.entrySet().stream().filter(map -> map.getKey() == orderId).collect(Collectors.toList());
+		// return list.stream().filter(list -> list.getId() == orderId).collect(Collectors.toList());
+
 	}
 }
