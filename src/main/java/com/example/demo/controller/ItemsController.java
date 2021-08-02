7c37704619b50b733b2803497edf15d7e387bd3c
Vishal Rana <vishal1230rana@gmail.com>
2021-08-02 14:40:32 +0530

    Param

--- src/main/java/com/example/demo/controller/ItemsController.java
+++ src/main/java/com/example/demo/controller/ItemsController.java
@@ -7,7 +7,7 @@ import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,13 +21,13 @@ public class ItemsController {
 		map.put(3, "wash");
 	}
 	
-	@GetMapping("/get") //http://localhost:5050/swagger-ui.html
+	@GetMapping("/") //http://localhost:5050/swagger-ui.html
 	public Map<Integer, String> getItems() {
 		return map;
 	}
 	
-	@GetMapping("/get/{tid}") // http://localhost:5050/swagger-ui.html/get/{tid}
-	public List<Entry<Integer, String>> getItems(@PathVariable("tid") int orderId) {
+	@GetMapping("/get") // http://localhost:5050/swagger-ui.html/get/{tid}
+	public List<Entry<Integer, String>> getItem(@RequestParam(value = "orderId") int orderId) {
 		return map.entrySet().stream().filter(map -> map.getKey() == orderId).collect(Collectors.toList());
 		// return list.stream().filter(list -> list.getId() == orderId).collect(Collectors.toList());
 
